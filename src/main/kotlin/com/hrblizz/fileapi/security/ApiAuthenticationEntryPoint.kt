73f4389764dd47e480e2272718ed8bdd90e6f9dc
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-04-11 16:57:28 +0300

    Remove custom ResponseEntity code

--- src/main/kotlin/com/hrblizz/fileapi/security/ApiAuthenticationEntryPoint.kt
+++ src/main/kotlin/com/hrblizz/fileapi/security/ApiAuthenticationEntryPoint.kt
@@ -1,11 +1,11 @@
 package com.hrblizz.fileapi.security
 
 import com.hrblizz.fileapi.library.JsonUtil
-import com.hrblizz.fileapi.rest.ResponseEntity
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
@@ -22,12 +22,7 @@ class ApiAuthenticationEntryPoint : AuthenticationEntryPoint {
         response.status = UNAUTHORIZED.value()
         response.contentType = MediaType.APPLICATION_JSON_VALUE
 
-        val responseEntity =
-            ResponseEntity(
-                null,
-                listOf(RestErrorMessage(authEx.message)),
-                UNAUTHORIZED.value()
-            )
+        val responseEntity = ResponseEntity(listOf(RestErrorMessage(authEx.message)), UNAUTHORIZED)
 
         response.writer.use { writer ->
             writer.println(JsonUtil.toJson(responseEntity))
