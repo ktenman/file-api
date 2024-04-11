9f274b5a49af1d03a633dd1879e51f7a7b3662f8
Konstantin Tenman <ktenman@users.noreply.github.com>
2024-04-11 16:52:40 +0300

    Remove redundant companion object and constructors in ResponseEntity.kt

--- src/main/kotlin/com/hrblizz/fileapi/rest/ResponseEntity.kt
+++ src/main/kotlin/com/hrblizz/fileapi/rest/ResponseEntity.kt
@@ -4,22 +4,4 @@ data class ResponseEntity<T>(
     val data: T? = null,
     val errors: List<ErrorMessage>? = null,
     val status: Int
-) {
-    companion object {
-        operator fun <T> invoke(status: Int): ResponseEntity<T> {
-            return ResponseEntity(null, null, status)
-        }
-
-        operator fun <T> invoke(data: T?, status: Int): ResponseEntity<T> {
-            return ResponseEntity(data, null, status)
-        }
-
-        operator fun <T> invoke(
-            data: T?,
-            errors: List<ErrorMessage>?,
-            status: Int
-        ): ResponseEntity<T> {
-            return ResponseEntity(data, errors, status)
-        }
-    }
-}
+)
